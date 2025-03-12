20ae2c29ef2c0144f042627af0d39d12f57e9091
Jefferson0k <jefersoncovenas7@gmail.com>
2025-03-11 21:58:46 -0500

    Payment Models, Loans

---
+++ app/Models/PrestamosModelo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PrestamosModelo extends Model{
+    protected $table = 'clientes';
+    protected $fillable = [
+
+    ];
+}
